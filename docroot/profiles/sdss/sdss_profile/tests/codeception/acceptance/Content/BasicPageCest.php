c73167733ca46892ac3c3cd8a52b1af376c2a942
Ian Monroe <ian@ianmonroe.com>
2023-05-30 10:17:16 -0600

    Created Related Content field.

--- docroot/profiles/sdss/sdss_profile/tests/codeception/acceptance/Content/BasicPageCest.php
+++ docroot/profiles/sdss/sdss_profile/tests/codeception/acceptance/Content/BasicPageCest.php
@@ -347,6 +347,16 @@ class BasicPageCest {
     $I->canSee('Last Updated: ' . $date_string);
   }
 
+  public function testRelatedContent(AcceptanceTester $I){
+    // A quick test to make sure it's only visible to administrators.
+    $I->logInWithRole('contributor');
+    $I->amOnPage('/node/add/stanford_page');
+    $I->cantSee('Related Content', 'h4');
+    $I->logInWithRole('administrator');
+    $I->amOnPage('/node/add/stanford_page');
+    $I->canSee('Related Content', 'h4');
+  }
+
   protected static function getTimezone() {
     return \Drupal::config('system.date')
       ->get('timezone.default') ?: @date_default_timezone_get();
