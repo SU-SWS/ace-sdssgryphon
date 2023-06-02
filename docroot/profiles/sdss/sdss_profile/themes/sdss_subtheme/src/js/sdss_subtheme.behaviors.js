732793ad72e5736f7a2ff1b16180ae0259496778
Jen Breese-Kauth <jbreese@stanford.edu>
2023-06-02 15:52:11 -0700

    fixup

--- docroot/profiles/sdss/sdss_profile/themes/sdss_subtheme/src/js/sdss_subtheme.behaviors.js
+++ docroot/profiles/sdss/sdss_profile/themes/sdss_subtheme/src/js/sdss_subtheme.behaviors.js
@@ -13,7 +13,6 @@
   attach (context, settings) {
     // console.log("Attached.");
 
-
     (function ($) {
 
       $('.su-brand-bar,.su-masthead').wrapAll('<div class="fixed-header">');
@@ -22,6 +21,25 @@
       var sdss_logo_classes = $('#block-sdss-subtheme-branding').attr('class');
       $('.fixed-header').addClass(sdss_logo_classes);
 
+      /**
+       * Open and close on the Newsroom menu
+       */
+
+      const newsMenus = $('.sdss-newsroom--menu .menu-item--expanded a');
+      newsMenus.removeAttr('href');
+
+      $(newsMenus, context).click(function(e) {
+
+        $(newsMenus).toggleClass('active');
+        $('#newsroom-menu--bg').toggleClass('active');
+
+        if ($(this).siblings('.menu-item--expanded').css('display') != 'none') {
+          $(this).attr('aria-expanded', 'true');
+        }
+        else {
+          $(this).attr('aria-expanded', 'false');
+        }
+      });
     })(jQuery);
   },
 
