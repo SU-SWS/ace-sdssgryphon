01724b68411e68c5815b69306c67325452ba80c5
Ian Monroe <ian@ianmonroe.com>
2023-06-12 10:53:44 -0600

    Update docroot/sites/sites.php
    Co-authored-by: Joe Gilliland-Lloyd <6943710+joegl@users.noreply.github.com>

--- docroot/sites/sites.php
+++ docroot/sites/sites.php
@@ -97,7 +97,6 @@ $sites['earthsystems.stanford.edu'] = 'esys';
 $sites['epsci.stanford.edu'] = 'gs';
 $sites['energypostdoc.stanford.edu'] = 'sepf';
 $sites['understand-energy.stanford.edu'] = 'understandenergy';
-$sites['understandenergy.stanford.edu'] = 'understandenergy';
 
 // Include local sites.
 if (file_exists(__DIR__ . '/local.sites.php')) {
