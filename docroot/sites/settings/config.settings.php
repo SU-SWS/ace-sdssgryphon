4c66b29dfe184f861821244d4be0e3181df62166
joegl <joegl@users.noreply.github.com>
2023-07-27 13:06:29 -0500

    fixup!

--- docroot/sites/settings/config.settings.php
+++ docroot/sites/settings/config.settings.php
@@ -17,9 +17,9 @@ if (!EnvironmentDetector::isProdEnv()) {
   // Get the site name and replace:
   // - Underscores "_" with dashes "-".
   // - Double underscores "__" with dots ".".
-  $sitename = str_replace('_', '-', str_replace('__', '.', $site_name));
+  $site_alias = str_replace('_', '-', str_replace('__', '.', $site_name));
 
   // Set stage_file_proxy settings.
-  $config['stage_file_proxy.settings']['origin'] = "https://$sitename-prod.stanford.edu";
-  $config['stage_file_proxy.settings']['origin_dir'] = "sites/$sitename/files";
+  $config['stage_file_proxy.settings']['origin'] = "https://$site_alias-prod.stanford.edu";
+  $config['stage_file_proxy.settings']['origin_dir'] = "sites/$site_name/files";
 }
