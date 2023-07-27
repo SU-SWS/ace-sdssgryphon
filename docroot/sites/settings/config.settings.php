a034d310759e31c3a918459375d56fc13cc5053a
joegl <joegl@users.noreply.github.com>
2023-07-27 11:52:29 -0500

    fixup!

--- docroot/sites/settings/config.settings.php
+++ docroot/sites/settings/config.settings.php
@@ -13,12 +13,7 @@ if (!EnvironmentDetector::isProdEnv()) {
 
 // Set stage_file_proxy URL on non-production environments.
 if (!EnvironmentDetector::isProdEnv()) {
-  // Get the site path.
-  $site_path = \Drupal\Core\DrupalKernel::findSitePath(\Drupal::request());
-  // Get site alias.
-  $site_path = explode('/', $site_path);
-  $site_alias = $site_path[1];
-  // Set stage_filx_proxy settings.
-  $config['stage_file_proxy.settings']['origin'] = "https://$site_alias.stanford.edu";
-  $config['stage_file_proxy.settings']['origin_dir'] = "sites/$site_alias/files";
+  // Set stage_file_proxy settings.
+  $config['stage_file_proxy.settings']['origin'] = "https://$site_name.stanford.edu";
+  $config['stage_file_proxy.settings']['origin_dir'] = "sites/$site_name/files";
 }
