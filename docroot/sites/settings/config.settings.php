c66cb9f6a2f183fd71c60b767ae34228b615d0ac
joegl <joegl@users.noreply.github.com>
2023-07-27 13:04:38 -0500

    Added alias underscore replacement to stage_file_proxy settings.

--- docroot/sites/settings/config.settings.php
+++ docroot/sites/settings/config.settings.php
@@ -13,7 +13,13 @@ if (!EnvironmentDetector::isProdEnv()) {
 
 // Set stage_file_proxy URL on non-production environments.
 if (!EnvironmentDetector::isProdEnv()) {
+
+  // Get the site name and replace:
+  // - Underscores "_" with dashes "-".
+  // - Double underscores "__" with dots ".".
+  $sitename = str_replace('_', '-', str_replace('__', '.', $site_name));
+
   // Set stage_file_proxy settings.
-  $config['stage_file_proxy.settings']['origin'] = "https://$site_name.stanford.edu";
-  $config['stage_file_proxy.settings']['origin_dir'] = "sites/$site_name/files";
+  $config['stage_file_proxy.settings']['origin'] = "https://$sitename-prod.stanford.edu";
+  $config['stage_file_proxy.settings']['origin_dir'] = "sites/$sitename/files";
 }
