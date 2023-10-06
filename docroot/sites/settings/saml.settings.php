49b53c1397de67573e8425b34727d8f84cb46dec
joegl <20383-joegl@users.noreply.drupalcode.org>
2023-10-06 16:39:01 -0500

    Adjusted SAML configuration to match paths.

--- docroot/sites/settings/saml.settings.php
+++ docroot/sites/settings/saml.settings.php
@@ -2,25 +2,30 @@
 
 /**
  * @file
- * Simplesamlphp config settings.
+ * SAML config settings.
  */
 
 use Acquia\Blt\Robo\Common\EnvironmentDetector;
 
-// Remove this when simplesamlphp is removed.
+// SimpleSAMLphp configuration
+// Set the workgroup api cert paths.
 $config['stanford_ssp.settings'] = [
-  'use_workgroup_api' => TRUE,
-  'workgroup_api_cert' => EnvironmentDetector::getAhFilesRoot() . '/nobackup/simplesamlphp/workgroup_api.cert',
-  'workgroup_api_key' => EnvironmentDetector::getAhFilesRoot() . '/nobackup/simplesamlphp/workgroup_api.key',
+  'workgroup_api_cert' => DRUPAL_ROOT . "/../keys/saml/workgroup_api.cert",
+  'workgroup_api_key' => DRUPAL_ROOT . "/../keys/saml/workgroup_api.key",
 ];
 
-// Always show local login when not on Acquia.
-if (!EnvironmentDetector::isAhEnv()) {
-  $config['stanford_ssp.settings']['hide_local_login'] = FALSE;
-  $config['stanford_samlauth.settings']['hide_local_login'] = FALSE;
+if (EnvironmentDetector::isAhEnv()) {
+  $group = EnvironmentDetector::getAhGroup();
+  $environment = EnvironmentDetector::getAhEnv();
+
+  // SimpleSAMLphp configuration
+  // Set the workgroup api cert paths.
+  $config['stanford_ssp.settings'] = [
+    'workgroup_api_cert' => "/mnt/gfs/$group.$environment/nobackup/apikeys/saml/workgroup_api.cert",
+    'workgroup_api_key' => "/mnt/gfs/$group.$environment/nobackup/apikeys/saml/workgroup_api.key",
+  ];
 }
 
-// Remove this when simplesamlphp is removed.
 $config['simplesamlphp_auth.settings'] = [
   'langcode' => 'en',
   'default_langcode' => 'en',
@@ -50,40 +55,29 @@ $config['simplesamlphp_auth.settings'] = [
   ],
 ];
 
+
 // Don't enable SAML configs if we're on CI systems.
 if (!EnvironmentDetector::isCiEnv()) {
-  $env = EnvironmentDetector::getAhEnv() ?: '';
-  $normalized_env = "01dev";
-  $idp = 'https://idp-uat.stanford.edu/';
-  $login = 'https://login-uat.stanford.edu/idp/profile/SAML2/Redirect/SSO';
-
-  if (EnvironmentDetector::isAhStageEnv()) {
-    $normalized_env = "01test";
-  }
-  elseif (EnvironmentDetector::isAhProdEnv()) {
-    $normalized_env = "01live";
-    $idp = 'https://idp.stanford.edu/';
-    $login = 'https://login.stanford.edu/idp/profile/SAML2/Redirect/SSO';
-  }
+  $idp = 'https://idp.stanford.edu/';
+  $login = 'https://login.stanford.edu/idp/profile/SAML2/Redirect/SSO';
 
   $config['samlauth.authentication'] = [
     'user_name_attribute' => 'uid',
-    'idp_entity_id' => $idp,
-    'sp_entity_id' => "https://acquiacloudsitefactory.stanford.edu/$normalized_env",
-    'idp_single_sign_on_service' => $login,
-    'sp_x509_certificate' => 'file:' . EnvironmentDetector::getAhFilesRoot() . '/nobackup/simplesamlphp/saml.crt',
-    'sp_private_key' => 'file:' . EnvironmentDetector::getAhFilesRoot() . '/nobackup/simplesamlphp/saml.pem',
+    'idp_entity_id' => 'https://idp.stanford.edu/',
+    'sp_entity_id' => 'https://swshumsci.stanford.edu',
+    'idp_single_sign_on_service' => 'https://login.stanford.edu/idp/profile/SAML2/Redirect/SSO',
+    'sp_x509_certificate' => 'file:' . EnvironmentDetector::getAhFilesRoot() . '/nobackup/apikeys/saml/cert/saml.crt',
+    'sp_private_key' => 'file:' . EnvironmentDetector::getAhFilesRoot() . '/nobackup/apikeys/saml/cert/saml.pem',
     'idp_certs' => [
-      'file:' . EnvironmentDetector::getAhFilesRoot() . '/nobackup/simplesamlphp/signing.crt',
+      'file:' . EnvironmentDetector::getAhFilesRoot() . '/nobackup/apikeys/saml/cert/signing.crt',
     ],
   ];
   $config['stanford_samlauth.settings'] = [
     'role_mapping' => [
       'workgroup_api' => [
-        'cert' => EnvironmentDetector::getAhFilesRoot() . '/nobackup/simplesamlphp/workgroup_api.cert',
-        'key' => EnvironmentDetector::getAhFilesRoot() . '/nobackup/simplesamlphp/workgroup_api.key',
+        'cert' => EnvironmentDetector::getAhFilesRoot() . '/nobackup/apikeys/saml/workgroup_api.cert',
+        'key' => EnvironmentDetector::getAhFilesRoot() . '/nobackup/apikeys/saml/workgroup_api.key',
       ],
     ],
   ];
 }
-$config['stanford_samlauth.settings']['allowed']['groups'][99] = 'uit:sws';
