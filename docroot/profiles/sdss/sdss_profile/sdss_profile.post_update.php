124d2e48bee587f6bd2912c5f9c16cd786ff30de
joegl <20383-joegl@users.noreply.drupalcode.org>
2024-11-07 12:32:45 -0600

    Implement hook_removed_post_updates.

--- docroot/profiles/sdss/sdss_profile/sdss_profile.post_update.php
+++ docroot/profiles/sdss/sdss_profile/sdss_profile.post_update.php
@@ -5,3 +5,15 @@
  * sdss_profile.post_update
  */
 
+/**
+ * Implements hook_removed_post_updates().
+ */
+function sdss_profile_removed_post_updates() {
+  return [
+    'sdss_profile_post_update_8200' => '4.7.0',
+    'sdss_profile_post_update_8202' => '4.7.0',
+    'sdss_profile_post_update_update_field_defs' => '4.7.0',
+    'sdss_profile_post_update_samlauth' => '4.7.0',
+    'sdss_profile_post_update_site_orgs' => '4.7.0',
+  ];
+}
