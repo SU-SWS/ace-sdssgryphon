da11f674fd51422ecf1b63ae7a141f73dd639f7c
Mike Decker <pookmish@stanford.edu>
2025-02-05 13:25:43 -0800

    SDSS-1452: New local video media type and added to image banner paragraph

--- docroot/profiles/sdss/sdss_profile/themes/sdss_subtheme/js/scripts.js
+++ docroot/profiles/sdss/sdss_profile/themes/sdss_subtheme/js/scripts.js
@@ -1,4 +1,4 @@
-(function ($, Drupal) {
+(function ($, Drupal, once) {
   'use strict';
   Drupal.behaviors.sdss_subtheme = {
     attach: function (context) {
@@ -8,9 +8,47 @@
 
       // Add current path as a drupal redirect desitnation to saml_login links.
       // Will redirect the user to the current page after logging in.
-      $('a[href="/saml_login"', context).attr("href", "/saml/login?destination=" + window.location.pathname);
+      $('a[href="/saml_login"', context).attr('href', '/saml/login?destination=' + window.location.pathname);
 
-    }
+      $(once('banner-video', '.ds-entity--stanford-banner video', context)).each(function (i) {
+        const video = this;
+        const videoId = `video-${i}`;
+        const $playPauseLabel = $('<span>').addClass('visually-hidden');
+
+        const $playPauseButton = $('<button>')
+          .html($playPauseLabel)
+          .addClass('fas play-pause-button')
+          .attr('aria-controls', videoId)
+          .attr('aria-describedby', `${videoId}-description`)
+          .click(function () {
+            const $button = $(this);
+            if (video.paused) {
+              $button.toggleClass('fa-pause').toggleClass('fa-play');
+              $playPauseLabel.text('Pause');
+              video.play();
+            } else {
+              $button.toggleClass('fa-pause').toggleClass('fa-play');
+              $playPauseLabel.text('Play');
+              video.pause();
+            }
+          });
+        $($playPauseButton).after($playPauseLabel);
+
+        if ($(video).attr('autoplay') === 'autoplay') {
+          $playPauseButton.addClass('fa-pause');
+          $playPauseLabel.text('Pause');
+        } else {
+          $playPauseButton.addClass('fa-play');
+          $playPauseLabel.text('Play');
+        }
+
+        const $container = $('<div>').addClass('video-info')
+          .append($playPauseButton)
+          .append($('<p>').attr('id', `${videoId}-description`).text('This video does not contain audio'));
+
+        $(video).attr('id', videoId).after($container);
+      });
+    },
   };
 
-})(jQuery, Drupal);
+})(jQuery, Drupal, once);
