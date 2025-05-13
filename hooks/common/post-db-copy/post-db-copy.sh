405bcd60aea7b8cb1bf8317fe0ca2d8fc353bb1d
joegl <6943710+joegl@users.noreply.github.com>
2025-05-13 13:52:22 -0500

    Updated comments.

--- hooks/common/post-db-copy/post-db-copy.sh
+++ hooks/common/post-db-copy/post-db-copy.sh
@@ -19,9 +19,12 @@ repo_root="/var/www/html/$site.$target_env"
 export PATH=$repo_root/vendor/bin:$PATH
 cd $repo_root
 
+# This command is the default and only runs updates if the site is on an ODE
+# environment.
 #blt artifact:ac-hooks:post-db-copy $site $target_env $db_name $source_env --environment=$target_env -v --no-interaction -D drush.ansi=false
+
 # Run the BLT command to run deploy hooks, including updating the database and
-# importing configuration.
+# importing configuration, on the target environment and site.
 blt artifact:update:drupal --site=$db_name --environment=$target_env
 
 set +v
