6290dc397468ea2ce1e5f5b132dd362647a9fd83
joegl <6943710+joegl@users.noreply.github.com>
2025-06-18 14:51:47 -0500

    Added desktop hamburger option to theme settings.

--- docroot/profiles/sdss/sdss_profile/themes/sdss_subtheme/theme-settings.php
+++ docroot/profiles/sdss/sdss_profile/themes/sdss_subtheme/theme-settings.php
@@ -29,6 +29,13 @@ function sdss_subtheme_form_system_theme_settings_alter(array &$form, FormStateI
     '#empty_option' => t('Green header, Bottom navigation'),
     '#default_value' => theme_get_setting('header_layout_variant'),
   ];
+
+  $form['options_settings']['sdss_subtheme_header_options']['desktop_hamburger'] = [
+    '#type' => 'checkbox',
+    '#title' => t('Use Hamburger Menu on Desktop'),
+    '#default_value' => theme_get_setting('desktop_hamburger'),
+    '#description' => t('Use a hamburger (collapsible) menu on desktop instead of the regular menu with drop-down navigation.'),
+  ];
 }
 
 // @codeCoverageIgnoreEnd
