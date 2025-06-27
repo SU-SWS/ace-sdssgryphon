f277dce2d0d04a2315eeaac834532d34b8731c34
joegl <6943710+joegl@users.noreply.github.com>
2025-06-27 10:10:31 -0500

    Added blt drupal:cron command.

--- blt/src/Blt/Plugin/Commands/GryphonCommands.php
+++ blt/src/Blt/Plugin/Commands/GryphonCommands.php
@@ -3,6 +3,8 @@
 namespace Gryphon\Blt\Plugin\Commands;
 
 use Acquia\Blt\Robo\BltTasks;
+use Acquia\Blt\Robo\Common\EnvironmentDetector;
+use GuzzleHttp\Client;
 
 /**
  * Class GryphonCommands.
@@ -35,12 +37,48 @@ class GryphonCommands extends BltTasks {
   }
 
   /**
+   * Run the Drupal cron on all sites.
+   *
+   * @command drupal:cron
+   */
+  public function cron() {
+    $this->config->set('drush.alias', '');
+    $failed = [];
+    foreach ($this->getConfigValue('multisites') as $multisite) {
+      try {
+        $this->say("Running Cron on <comment>$multisite</comment>...");
+        $this->switchSiteContext($multisite);
+
+        $task = $this->taskDrush()
+          ->drush("cron")
+          ->run();
+        if (!$task->wasSuccessful()) {
+          $failed[] = $multisite;
+          $this->say("Cron on <comment>$multisite</comment> failed.");
+          continue;
+        }
+        $this->say("Cron on <comment>$multisite</comment> completed successfully.");
+      }
+      catch (\Exception $e) {
+        $this->say("Unable to run cron on <comment>$multisite</comment>: " . $e->getMessage());
+        $failed[] = $multisite;
+        continue;
+      }
+    }
+
+    if ($failed) {
+      $this->say("Cron failed on the following sites: " . implode(', ', $failed));
+    }
+
+    $this->say("Cron completed successfully on all sites.");
+  }
+
+  /**
    * Generate a list of emails for the given role on all sites.
    *
    * @command sdss:role-report
    */
-  public function roleReport($role)
-  {
+  public function roleReport($role) {
     $information = [];
     foreach ($this->getConfigValue('multisites') as $site) {
       $emails = $this->taskDrush()
