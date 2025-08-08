96c8d0ca3f59880380e1abc97a6c075d2950f708
joegl <6943710+joegl@users.noreply.github.com>
2025-08-08 16:14:40 -0500

    Update unit tests.

--- docroot/profiles/sdss/sdss_profile/modules/sdss_person_title_override/tests/src/Unit/Plugin/Field/FieldFormatter/PersonTitleOverrideFormatterTest.php
+++ docroot/profiles/sdss/sdss_profile/modules/sdss_person_title_override/tests/src/Unit/Plugin/Field/FieldFormatter/PersonTitleOverrideFormatterTest.php
@@ -2,19 +2,24 @@
 
 declare(strict_types=1);
 
-namespace Drupal\sdss_person_title_override\Tests\Plugin\Field\FieldFormatter;
+namespace Drupal\Tests\sdss_person_title_override\Unit\Plugin\Field\FieldFormatter;
 
 use Drupal\sdss_person_title_override\Plugin\Field\FieldFormatter\PersonTitleOverrideFormatter;
 use PHPUnit\Framework\TestCase;
 
-class PersonTitleOverrideFormatterTest extends TestCase
-{
-    public function testFormatterReturnsExpectedValue()
-    {
-        $formatter = $this->getMockBuilder(PersonTitleOverrideFormatter::class)
-            ->disableOriginalConstructor()
-            ->getMock();
-        // You would add more detailed tests here, possibly using dependency injection and mocking services.
-        $this->assertInstanceOf(PersonTitleOverrideFormatter::class, $formatter);
-    }
+/**
+ * Test the SDSS Person Title Override Field Formatter.
+ *
+ * @group sdss_person_title_override
+ * @coversDefaultClass \Drupal\sdss_person_title_override\Plugin\Field\FieldFormatter\PersonTitleOverrideFormatter
+ */
+class PersonTitleOverrideFormatterTest extends TestCase {
+
+  public function testFormatterReturnsExpectedValue() {
+    $formatter = $this->getMockBuilder(PersonTitleOverrideFormatter::class)
+        ->disableOriginalConstructor()
+        ->getMock();
+    // You would add more detailed tests here, possibly using dependency injection and mocking services.
+    $this->assertInstanceOf(PersonTitleOverrideFormatter::class, $formatter);
+  }
 }
--- docroot/profiles/sdss/sdss_profile/modules/sdss_person_title_override/tests/src/Unit/Service/SdssPersonTitleServiceTest.php
+++ docroot/profiles/sdss/sdss_profile/modules/sdss_person_title_override/tests/src/Unit/Service/SdssPersonTitleServiceTest.php
@@ -2,109 +2,129 @@
 
 declare(strict_types=1);
 
-namespace Drupal\sdss_person_title_override\Tests\Service;
+namespace Drupal\Tests\sdss_person_title_override\Unit\Service;
 
 use Drupal\sdss_person_title_override\Service\SdssPersonTitleService;
+use Drupal\node\NodeInterface;
 use PHPUnit\Framework\TestCase;
 
+/**
+ * Test the SDSS Person Title Override Service.
+ *
+ * @group sdss_person_title_override
+ * @coversDefaultClass \Drupal\sdss_person_title_override\Service\SdssPersonTitleService
+ */
 class SdssPersonTitleServiceTest extends TestCase {
-    private function getNodeMock($bundle, $fields) {
-        $node = $this->getMockBuilder('Drupal\\node\\NodeInterface')
-            ->disableOriginalConstructor()
-            ->getMock();
-        $node->method('bundle')->willReturn($bundle);
-        $node->method('hasField')->willReturnCallback(function($field) use ($fields) {
-            return array_key_exists($field, $fields);
-        });
-        $node->method('get')->willReturnCallback(function($field) use ($fields) {
-            return (object) ['value' => $fields[$field]];
-        });
-        return $node;
-    }
 
-    public function testGetDisplayFullTitleReturnsExpectedValue() {
-        $service = new SdssPersonTitleService();
-        $node = (object) [
-            'field_title' => [0 => ['value' => 'Professor']],
-            'field_alt_title' => [0 => ['value' => '']],
-        ];
-        $result = $service->getDisplayFullTitle($node);
-        $this->assertEquals('Professor', $result);
-    }
+  /**
+   * Creates a NodeInterface mock with the given bundle and field values.
+   *
+   * @param string $bundle
+   *   The node bundle name.
+   * @param array $fields
+   *   An associative array of field names and their values.
+   *
+   * @return \Drupal\node\NodeInterface
+   *   The mocked node object.
+   */
+  private function getNodeMock($bundle, $fields) {
+    $node = $this->getMockBuilder(NodeInterface::class)
+      ->disableOriginalConstructor()
+      ->getMock();
+    $node->method('bundle')->willReturn($bundle);
+    $node->method('hasField')->willReturnCallback(function($field) use ($fields) {
+      return array_key_exists($field, $fields);
+    });
+    $node->method('get')->willReturnCallback(function($field) use ($fields) {
+      return (object) ['value' => $fields[$field]];
+    });
+    return $node;
+  }
 
-    public function testGetDisplayShortTitleReturnsExpectedValue() {
-        $service = new SdssPersonTitleService();
-        $node = (object) [
-            'field_title' => [0 => ['value' => 'Professor']],
-            'field_alt_title' => [0 => ['value' => 'Dr.']],
-        ];
-        $result = $service->getDisplayShortTitle($node);
-        $this->assertEquals('Dr.', $result);
-    }
+  /**
+   * Tests that getDisplayFullTitle returns the full title when alt title is 
+   * empty.
+   */
+  public function testGetDisplayFullTitleReturnsExpectedValue() {
+    $service = new SdssPersonTitleService();
+    $fields = [
+      'su_sdss_person_alt_title' => '',
+      'su_person_full_title' => 'Professor',
+    ];
+    $node = $this->getNodeMock('stanford_person', $fields);
+    $result = $service->getDisplayFullTitle($node);
+    $this->assertEquals('Professor', $result);
+  }
 
-    public function testGetDisplayFullTitleReturnsAltTitleIfPresent() {
-        $service = new SdssPersonTitleService();
-        $fields = [
-            'su_sdss_person_alt_title' => 'Dr.',
-            'su_person_full_title' => 'Professor',
-        ];
-        $node = $this->getNodeMock('stanford_person', $fields);
-        $result = $service->getDisplayFullTitle($node);
-        $this->assertEquals('Dr.', $result);
-    }
+  /**
+   * Tests that getDisplayFullTitle returns the alt title if present.
+   */
+  public function testGetDisplayFullTitleReturnsAltTitleIfPresent() {
+    $service = new SdssPersonTitleService();
+    $fields = [
+      'su_sdss_person_alt_title' => 'Dr.',
+      'su_person_full_title' => 'Professor',
+    ];
+    $node = $this->getNodeMock('stanford_person', $fields);
+    $result = $service->getDisplayFullTitle($node);
+    $this->assertEquals('Dr.', $result);
+  }
 
-    public function testGetDisplayFullTitleReturnsFullTitleIfNoAltTitle() {
-        $service = new SdssPersonTitleService();
-        $fields = [
-            'su_sdss_person_alt_title' => '',
-            'su_person_full_title' => 'Professor',
-        ];
-        $node = $this->getNodeMock('stanford_person', $fields);
-        $result = $service->getDisplayFullTitle($node);
-        $this->assertEquals('Professor', $result);
-    }
+  /**
+   * Tests that getDisplayShortTitle returns the alt title when present.
+   */
+  public function testGetDisplayShortTitleReturnsExpectedValue() {
+    $service = new SdssPersonTitleService();
+    $fields = [
+      'su_sdss_person_alt_title' => 'Dr.',
+      'su_person_short_title' => 'Prof.',
+    ];
+    $node = $this->getNodeMock('stanford_person', $fields);
+    $result = $service->getDisplayShortTitle($node);
+    $this->assertEquals('Dr.', $result);
+  }
 
-    public function testGetDisplayShortTitleReturnsAltTitleIfPresent() {
-        $service = new SdssPersonTitleService();
-        $fields = [
-            'su_sdss_person_alt_title' => 'Dr.',
-            'su_person_short_title' => 'Prof.',
-        ];
-        $node = $this->getNodeMock('stanford_person', $fields);
-        $result = $service->getDisplayShortTitle($node);
-        $this->assertEquals('Dr.', $result);
-    }
+  /**
+   * Tests that getDisplayShortTitle returns the short title if alt title is
+   * empty.
+   */
+  public function testGetDisplayShortTitleReturnsShortTitleIfNoAltTitle() {
+    $service = new SdssPersonTitleService();
+    $fields = [
+      'su_sdss_person_alt_title' => '',
+      'su_person_short_title' => 'Prof.',
+    ];
+    $node = $this->getNodeMock('stanford_person', $fields);
+    $result = $service->getDisplayShortTitle($node);
+    $this->assertEquals('Prof.', $result);
+  }
 
-    public function testGetDisplayShortTitleReturnsShortTitleIfNoAltTitle() {
-        $service = new SdssPersonTitleService();
-        $fields = [
-            'su_sdss_person_alt_title' => '',
-            'su_person_short_title' => 'Prof.',
-        ];
-        $node = $this->getNodeMock('stanford_person', $fields);
-        $result = $service->getDisplayShortTitle($node);
-        $this->assertEquals('Prof.', $result);
-    }
+  /**
+   * Tests that getDisplayFullTitle throws an exception for a
+   * non-stanford_person bundle.
+   */
+  public function testGetDisplayFullTitleThrowsExceptionForWrongBundle() {
+    $service = new SdssPersonTitleService();
+    $fields = [
+      'su_sdss_person_alt_title' => 'Dr.',
+      'su_person_full_title' => 'Professor',
+    ];
+    $node = $this->getNodeMock('other_bundle', $fields);
+    $this->expectException(\LogicException::class);
+    $service->getDisplayFullTitle($node);
+  }
 
-    public function testGetDisplayFullTitleThrowsExceptionForWrongBundle() {
-        $service = new SdssPersonTitleService();
-        $fields = [
-            'su_sdss_person_alt_title' => 'Dr.',
-            'su_person_full_title' => 'Professor',
-        ];
-        $node = $this->getNodeMock('other_bundle', $fields);
-        $this->expectException(\LogicException::class);
-        $service->getDisplayFullTitle($node);
-    }
-
-    public function testGetDisplayShortTitleThrowsExceptionForMissingFields() {
-        $service = new SdssPersonTitleService();
-        $fields = [
-            'su_sdss_person_alt_title' => 'Dr.'
-            // su_person_short_title missing
-        ];
-        $node = $this->getNodeMock('stanford_person', $fields);
-        $this->expectException(\LogicException::class);
-        $service->getDisplayShortTitle($node);
-    }
+  /**
+   * Tests that getDisplayShortTitle throws an exception when required fields
+   * are missing.
+   */
+  public function testGetDisplayShortTitleThrowsExceptionForMissingFields() {
+    $service = new SdssPersonTitleService();
+    $fields = [
+      'su_sdss_person_alt_title' => 'Dr.'
+    ];
+    $node = $this->getNodeMock('stanford_person', $fields);
+    $this->expectException(\LogicException::class);
+    $service->getDisplayShortTitle($node);
+  }
 }
