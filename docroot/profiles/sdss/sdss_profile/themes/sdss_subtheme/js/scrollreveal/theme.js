7ace476e0c0f878642b913f36b91ca7188a5b36c
jackclimber <jack@climber.io>
2025-09-12 17:40:00 -0400

    sdssgryphon-10: Style fixes for the new homepage.

--- docroot/profiles/sdss/sdss_profile/themes/sdss_subtheme/js/scrollreveal/theme.js
+++ docroot/profiles/sdss/sdss_profile/themes/sdss_subtheme/js/scrollreveal/theme.js
@@ -1,8 +1,29 @@
-document.addEventListener('DOMContentLoaded', function() {
+window.addEventListener('load', function() {
     const sr = ScrollReveal();
-    sr.reveal('.su-intro-text, .su-wysiwyg-text h2, .su-wysiwyg-text h3, .su-wysiwyg-text p', { distance: '50px',
+    sr.reveal('.su-wysiwyg-text h2', { 
+        distance: '80px',
         origin: 'right',
+        duration: 700,
+        delay: 500,
+        easing: 'ease-in-out',
+    });
+    sr.reveal('.ptype-stanford-lists img', { 
+        distance: '0',
+        delay: 500,
+        duration: 700,
+        easing: 'ease-out',
+    });
+    /*sr.reveal('.layout--layout-paragraphs-two-column .su-card img, .layout--layout-paragraphs-two-column .su-card h2, .layout--layout-paragraphs-two-column .su-card .su-card__button', { 
+        distance: '50px',
         duration: 1000,
-        easing: 'ease-out'
+        interval: 100,
+        easing: 'ease-out',
+    });*/
+    sr.reveal('.layout--layout-paragraphs-two-column .su-card, .layout--layout-paragraphs-three-column .su-card', { 
+        distance: '80px',
+        duration: 600,
+        delay: 500,
+        interval: 200,
+        easing: 'ease-out',
     });
 });
