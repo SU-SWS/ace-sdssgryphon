a25700a3cc5b31c4f340a1db02473cbdfa4cdeb2
jackclimber <jack@climber.io>
2025-09-16 14:22:32 -0400

    sdssgryphon-12: Fixed scrolltrigger and mobile bugs.

--- docroot/profiles/sdss/sdss_profile/themes/sdss_subtheme/js/scrollreveal/theme.js
+++ docroot/profiles/sdss/sdss_profile/themes/sdss_subtheme/js/scrollreveal/theme.js
@@ -1,29 +1,58 @@
 window.addEventListener('load', function() {
-    const sr = ScrollReveal();
-    sr.reveal('.su-wysiwyg-text h2', { 
-        distance: '80px',
-        origin: 'right',
-        duration: 700,
-        delay: 200,
-        easing: 'ease-in-out',
-    });
-    sr.reveal('.ptype-stanford-lists img', { 
-        distance: '0',
-        delay: 2500,
-        duration: 700,
-        easing: 'ease-out',
-    });
-    /*sr.reveal('.layout--layout-paragraphs-two-column .su-card img, .layout--layout-paragraphs-two-column .su-card h2, .layout--layout-paragraphs-two-column .su-card .su-card__button', { 
-        distance: '50px',
-        duration: 1000,
-        interval: 100,
-        easing: 'ease-out',
-    });*/
-    sr.reveal('.layout--layout-paragraphs-three-column .su-card', { 
-        distance: '80px',
-        duration: 600,
-        delay: 200,
-        interval: 200,
-        easing: 'ease-out',
-    });
+    this.setTimeout(() => {
+        for(const section of document.querySelectorAll('.su-wysiwyg-text')) {
+            gsap.fromTo(section.querySelector('h2'), 
+                { opacity: 0, x: 80 }, 
+                { 
+                    opacity: 1, 
+                    x: 0, 
+                    duration: .7, 
+                    delay: .2, 
+                    ease: 'power2.inOut',
+                    scrollTrigger: {
+                        trigger: section,
+                        start: "top 80%", // later in the viewport
+                        toggleActions: "play none none none",
+                        once: true,
+                    }
+                }
+            );
+        }
+        for(const section of document.querySelectorAll('.ptype-stanford-lists .su-card')) {
+            gsap.fromTo(section.querySelector('img'), 
+                { opacity: 0 }, 
+                { 
+                    opacity: 1,
+                    duration: .7, 
+                    delay: .2, 
+                    ease: 'power2.inOut',
+                    scrollTrigger: {
+                        trigger: section,
+                        start: "top 80%", // later in the viewport
+                        toggleActions: "play none none none",
+                        once: true,
+                    }
+                }
+            );
+        }
+        for(const section of document.querySelectorAll('.layout--layout-paragraphs-three-column')) {
+            gsap.fromTo(section.querySelectorAll('.su-card'), 
+                { opacity: 0, y: 80 }, 
+                { 
+                    opacity: 1,
+                    duration: .6, 
+                    y: 0,
+                    delay: .2, 
+                    ease: 'power2.inOut',
+                    stagger: .2,
+                    scrollTrigger: {
+                        trigger: section,
+                        start: "top 80%", // later in the viewport
+                        toggleActions: "play none none none",
+                        once: true,
+                    }
+                }
+            );
+        }
+    }, 1000);
 });
