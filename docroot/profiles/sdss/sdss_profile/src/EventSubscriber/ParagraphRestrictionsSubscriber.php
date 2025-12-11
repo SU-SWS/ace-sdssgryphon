73b9e555b9f52a74699620a002b6a707da016fc4
jackclimber <jack@climber.io>
2025-12-11 13:18:48 -0500

    Accelerator fix

--- docroot/profiles/sdss/sdss_profile/src/EventSubscriber/ParagraphRestrictionsSubscriber.php
+++ docroot/profiles/sdss/sdss_profile/src/EventSubscriber/ParagraphRestrictionsSubscriber.php
@@ -107,7 +107,7 @@ class ParagraphRestrictionsSubscriber implements EventSubscriberInterface {
     ];
 
     // Unset paragraph types if not on the Sustainability site.
-    if ($this->isSustainabilitySite() === FALSE) {
+    if ($this->isAllowedSite() === FALSE) {
       $types = $this->unsetParagraphTypes($event->getTypes(), $sustainability_home_paragraphs);
       $event->setTypes($types);
       return;
@@ -138,10 +138,14 @@ class ParagraphRestrictionsSubscriber implements EventSubscriberInterface {
    * @return bool
    *   TRUE if the site is 'sustainability', FALSE otherwise.
    */
-  protected function isSustainabilitySite(): bool {
+  protected function isAllowedSite(): bool {
     $site_path = \Drupal::getContainer()->getParameter('site.path');
     $site_name = basename($site_path);
-    return $site_name === 'sustainability';
+    $acceptableSites = array(
+      'sustainability',
+      'sustainability-accelerator',
+    );
+    return in_array($site_name, $acceptableSites);
   }
 
 }
